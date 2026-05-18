165639d52b2b899f1e88092fb30dba430daf3d28
iasemin005-arch <iasemin005@gmail.com>
2026-05-18 10:04:57 +0600

    Update urls.py

--- yasemin_salon/urls.py
+++ yasemin_salon/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import re
 
-admin.site.site_header = 'Ясемин — Управление'
-admin.site.site_title = 'Ясемин Салон'
+admin.site.site_header = 'Подружки — Управление'
+admin.site.site_title = 'Подружки Салон'
 admin.site.index_title = 'Панель управления'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('salon.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
